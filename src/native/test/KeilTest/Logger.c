a6d12c298014af99c12d1efe29eb19216ae4aef8
Christian Hujer <cher@riedquat.de>
2014-05-02 00:30:59 +0200

    Remove some compiler warnings for C251.

--- src/native/test/KeilTest/Logger.c
+++ src/native/test/KeilTest/Logger.c
@@ -7,52 +7,6 @@ void logRunnerStarted()
 }
 #endif
 
-#ifdef ACEUNIT_LOG_SUITE
-/** @see TestLogger_t.suiteStarted */
-void logSuiteStarted()
-{
-}
-#endif
-
-#ifdef ACEUNIT_LOG_FIXTURE
-/** @see TestLogger_t.fixtureStarted */
-void logFixtureStarted(const FixtureId_t fixture)
-{
-}
-#endif
-
-#ifdef ACEUNIT_LOG_TESTCASE
-/** @see TestLogger_t.testCaseStarted */
-void logTestCaseStarted(TestCaseId_t testCase)
-{
-}
-#endif
-
-/** @see TestLogger_t.testCaseCailed */
-void logTestCaseFailed(const AssertionError_t *error)
-{
-}
-
-#ifdef ACEUNIT_LOG_TESTCASE
-/** @see TestLogger_t.testCaseEnded */
-void logTestCaseEnded(TestCaseId_t testCase)
-{
-}
-#endif
-
-#ifdef ACEUNIT_LOG_FIXTURE
-void logFixtureEnded(const FixtureId_t fixture)
-{
-}
-#endif
-
-#ifdef ACEUNIT_LOG_SUITE
-/** @see TestLogger_t.suiteEnded */
-void logSuiteEnded()
-{
-}
-#endif
-
 #ifdef ACEUNIT_LOG_RUNNER
 /** @see TestLogger_t.runnerEnded */
 void logRunnerEnded()
@@ -64,13 +18,13 @@ void logRunnerEnded()
 AceUnitNewLogger(
     loggerStub, /* CHANGE THIS NAME!!! */
     logRunnerStarted,
-    logSuiteStarted,
-    logFixtureStarted,
-    logTestCaseStarted,
-    logTestCaseFailed,
-    logTestCaseEnded,
-    logFixtureEnded,
-    logSuiteEnded,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
     logRunnerEnded
 );
 
