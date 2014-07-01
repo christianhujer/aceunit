932b3888464e6b719f05d43d477936307ceb79b2
Christian Hujer <cher@riedquat.de>
2014-07-01 19:02:37 +0200

    Improve KeilTest.

--- test/KeilTest/DummyTest.c
+++ test/KeilTest/DummyTest.c
@@ -1,5 +1,11 @@
 #include "DummyTest.h"
 
-A_Test void dummyTest(void)
+A_Test void failingTest(void)
 {
+    assertTrue("", false);
+}
+
+A_Test void succeedingTest(void)
+{
+    assertTrue("", true);
 }
