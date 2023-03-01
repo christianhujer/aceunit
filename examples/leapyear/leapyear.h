7764a632d63fd5793ad0a427150919a4efedf1a3
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-03-01 18:30:09 +0530

    Make example compatible with clang C90 which doesn't provide _Bool as backport (which is fine).

--- examples/leapyear/leapyear.h
+++ examples/leapyear/leapyear.h
@@ -1,7 +1,14 @@
 #ifndef LEAPYEAR_H
 #define LEAPYEAR_H
 
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
 #include <stdbool.h>
+#elif !defined(__bool_true_false_are_defined)
+#define bool int
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
+#endif
 
 extern bool isLeapYear(int year);
 
