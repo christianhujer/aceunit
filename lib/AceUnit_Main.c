272b87ab2ba501970337f1fc3dd700f94210dfff
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-03-02 20:32:43 +0530

    Use program name for summary message.

--- lib/AceUnit_Main.c
+++ lib/AceUnit_Main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L)
+#define A_Ignored [[maybe_unused]]
+#elif defined(__GNUC__)
+#define A_Ignored __attribute__((__unused__))
+#else
+#define A_Ignored
+#endif
+
+int main(A_Ignored int argc, char *argv[]) {
     AceUnit_Result_t result = { 0, 0, 0 };
     AceUnit_run(fixtures, &result);
-    fprintf(stderr, "AceUnit: %d test cases, %d successful, %d failed.\n", result.testCaseCount, result.successCount, result.failureCount);
+    fprintf(stderr, "%s: %d test cases, %d successful, %d failed.\n", argv[0], result.testCaseCount, result.successCount, result.failureCount);
     return result.failureCount ? EXIT_FAILURE : EXIT_SUCCESS;
 }
