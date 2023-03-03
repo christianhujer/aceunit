c6d2b6dd05891e6bd49cbd855a8e16d711ca7029
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-03-03 23:46:50 +0530

    Implement #19 with option -n for the fixture name.

---
+++ test/generatorTests/fixture_name/fixture1.c
@@ -0,0 +1,7 @@
+void beforeAll1(void) {}
+void beforeEach1(void) {}
+void afterEach1(void) {}
+void afterAll1(void) {}
+void test1_1(void) {}
+void test1_2(void) {}
+void test1_3(void) {}
---
+++ test/generatorTests/fixture_name/fixture2.c
@@ -0,0 +1,7 @@
+void beforeAll2(void) {}
+void beforeEach2(void) {}
+void afterEach2(void) {}
+void afterAll2(void) {}
+void test2_1(void) {}
+void test2_2(void) {}
+void test2_3(void) {}
