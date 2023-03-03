6e200f8764911bd58e950307d37676245c7607c2
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-03-03 23:29:50 +0530

    Use aceunit generator in AceUnit self-test.

--- lib/AceUnit_SetJmpRunner_Test.c
+++ lib/AceUnit_SetJmpRunner_Test.c
@@ -6,26 +6,13 @@ extern jmp_buf *AceUnit_env;
 
 bool ran_tc_ok = false;
 
-void tc_failing(void) {
+void test_failing(void) {
     assert(false);
 }
 
-void tc_ok(void) {
+void test_ok(void) {
     ran_tc_ok = true;
 }
-
-static void (*const failing_test[])(void) = {
-    tc_failing,
-    tc_ok,
-    NULL,
-};
-
-const AceUnit_Fixture_t failing_test_fixture = { NULL, NULL, NULL, NULL, failing_test };
-
-const AceUnit_Fixture_t *fixtures[] = {
-    &failing_test_fixture,
-    NULL,
-};
 
 int main(void) {
     AceUnit_Result_t result = { 0, 0, 0 };
