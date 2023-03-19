f01f6d0680c103ed63b2aaa676f44977c4d98149
Christian Hujer <Christian.Hujer@nelkinda.com>
2023-03-19 11:50:22 +0530

    Fix bug in exclude mechanism.

--- recurse.mk
+++ recurse.mk
@@ -1,4 +1,4 @@
-subs?=$(filter-out $(exclude-subs),$(patsubst %/Makefile,%,$(wildcard */Makefile)))
+subs?=$(filter-out $(exclude_subs),$(patsubst %/Makefile,%,$(wildcard */Makefile)))
 targets?=all clean
 
 define recurse_template
