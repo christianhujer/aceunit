f3f57cfd3a041ccf35680be05897b750f265b76d
Christian Hujer <Christian.Hujer@nelkinda.com>
2026-06-20 18:06:17 +0530

    doc: Fix broken example.

--- share/doc/aceunit/examples/hello_world_mocked_link/hello_test.c
+++ share/doc/aceunit/examples/hello_world_mocked_link/hello_test.c
@@ -2,9 +2,9 @@
 
 #include "mock_puts.h"
 
-extern int __real_main(void);
+extern int __real_main(int argc, char *const argv[]);
 
 void test_hello(void) {
-    __real_main();
+    __real_main(0, NULL);
     assert_puts("Hello, world!\n");
 }
